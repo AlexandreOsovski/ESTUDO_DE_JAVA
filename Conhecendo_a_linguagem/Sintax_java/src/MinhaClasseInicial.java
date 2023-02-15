abe98cc9762c7c1772cc05bb0db87b889e8fa0f3
Alexandre Osovski <74330383+AlexandreOsovski@users.noreply.github.com>
2023-02-15 16:47:15 -0300

    initial commit: iniciando estudos

---
+++ Conhecendo_a_linguagem/Sintax_java/src/MinhaClasseInicial.java
@@ -0,0 +1,12 @@
+public class MinhaClasseInicial {
+    public static void main(String[] args) {
+
+        System.out.println("olá mundo");
+
+    }
+
+    public static int calculadora(int a, int b) {
+        return a + b;
+    }
+
+}
