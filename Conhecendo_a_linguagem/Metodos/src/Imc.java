dce0a169f729b0bd4654f92d3bf37725f0bba1ef
Alexandre Osovski <74330383+AlexandreOsovski@users.noreply.github.com>
2023-02-16 07:48:14 -0300

    create: exemplo de código pouco identado

---
+++ Conhecendo_a_linguagem/Metodos/src/Imc.java
@@ -0,0 +1,32 @@
+public class Imc {
+
+    public static void main(String[] args) {
+
+        Double a = (double) 10;
+        Double b = (double) 20;
+
+        String resultado = codigoImcSemIdentacao(a, b);
+        System.out.println(resultado);
+    }
+
+    public static String codigoImcSemIdentacao(Double peso, Double altura) {
+
+        String resultado;
+
+        Double resultadoImc = peso / (altura * altura);
+
+        if (resultadoImc < 19) {
+
+            resultado = "Seu peso está baixo";
+
+        } else if (resultadoImc > 19 && resultadoImc < 25) {
+            resultado = "Seu peso é ideal";
+        } else {
+            resultado = "você está acima do peso";
+        }
+
+        return resultado;
+
+    }
+
+}
